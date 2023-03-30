b6e6f86235b2c900f502a55c306e69b8ab13751d
Nacho Cayuqueo <cayuqueoNacho@gmail.com>
2023-03-30 09:40:11 -0300

    unused import was deleted

--- src/hooks/useChechAuth.js
+++ src/hooks/useChechAuth.js
@@ -1,8 +1,7 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { FirebaseAuth } from "../firebase/config";
 
 import { login, logout } from "../store/auth";
 import { startLoadingNotes } from "../store/journal/thunks";
